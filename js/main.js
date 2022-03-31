0227d98bc08b2cf23e983997ecc32d34da233205
Jhon Rios <r.jhonf@gmail.com>
2022-03-31 17:18:12 -0500

    Agregando los estilos

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const ham = document.querySelector('.nav-bar__responsive--icon');
+const link = document.querySelector('.nav-bar__responsive-ul'); 
+const bars = document.querySelectorAll('.nav-bar__responsive--icon span');
+ham.addEventListener('click', () => {
+    link.classList.toggle('active');
+    bars.forEach(child => {
+        child.classList.toggle('rotate')
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
